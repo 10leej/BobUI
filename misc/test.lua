187a282111c4077818c0fea4937cac58e6f2cc0e
Joshua Lee <10leej@gmail.com>
2019-11-08 12:29:37 -0500

    testing some DF moving
    doesn't look like any of this is gonna work out

--- misc/test.lua
+++ misc/test.lua
@@ -3,3 +3,33 @@ local _, cfg = ... --import config
 local addon, ns = ... --get addon namespace
 
 --this is a file for developers that wants to test stuff.
+
+--[[ 
+--desperately trying to move the durability frame, guess it's not gonna happen
+local function DFmove()
+	print("Fired: Function 'DurabilityFrame_SetAlerts'") --debug
+	local f = CreateFrame("Frame", "BobDFmover", UIParent)
+	f:SetScript("OnEvent", function(self, event, unit)
+	f:UnregisterEvent(event)
+		print("Fired After Function: UPDATE_INVENTORY_DURABILITY")
+		DurabilityFrame:ClearAllPoints()
+		DurabilityFrame:SetPoint("CENTER",UIParent,"CENTER",0,0)
+		print("frame moved by event in function")
+	end)
+	f:RegisterEvent("PLAYER_LOGIN")
+	f:RegisterEvent('UPDATE_INVENTORY_DURABILITY')
+end
+hooksecurefunc("DurabilityFrame_SetAlerts", DFmove)
+
+local f = CreateFrame("Frame", "BobDFmover", UIParent)
+f:SetScript("OnEvent", function(self, event, unit)
+f:UnregisterEvent(event)
+	print("Fired Event")
+	DurabilityFrame:ClearAllPoints()
+	DurabilityFrame:SetPoint("CENTER",UIParent,"CENTER",0,0)
+	print("Frame moved by event")
+end)
+f:RegisterEvent("PLAYER_LOGIN")
+f:RegisterEvent('UPDATE_INVENTORY_DURABILITY')
+f:RegisterEvent('UNIT_INVENTORY_CHANGED')
+]]
